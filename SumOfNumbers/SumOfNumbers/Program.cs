0317099bd7c81ccf97d1326c56e50dd019b672a7
Belov-AI <Belov.Alexander@urfu.ru>
2019-10-28 17:44:14 +0500

    Сделано SumOfNumbers

---
+++ SumOfNumbers/SumOfNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            var sum = 0;
+            var count = 0;
+
+            Console.WriteLine("Введите максимальное значение суммы");
+            var upperBorder = int.Parse(Console.ReadLine());
+
+            while (sum < upperBorder)
+            {
+                Console.Write("Введите целое число: ");
+                var number = int.Parse(Console.ReadLine());
+                count++;
+                sum += number;
+            }
+
+            Console.WriteLine("Введено {0} чисел. Сумма равна {1}", count, sum);
+            Console.ReadKey();
+        }
+    }
+}
