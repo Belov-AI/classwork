6f964fd4ba840208070b46f102d948e40102df38
Belov-AI <Belov.Alexander@urfu.ru>
2019-10-28 17:28:38 +0500

    Сделан StringInFrame

---
+++ StringInFrame/StringInFrame/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace StringInFrame
+{
+    class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                Console.WriteLine("Введите строку (Enter - завершение работы)");
+                var str = Console.ReadLine();
+
+                if (str == "")
+                    break;
+
+                var number = str.Length + 2;
+
+                Console.WriteLine();
+                PrintHorizontalBorder(number);
+                Console.WriteLine("| {0} |", str);
+                PrintHorizontalBorder(number);
+                Console.WriteLine();
+            }
+            
+            //Console.ReadKey();
+        }
+
+        static void PrintHorizontalBorder(int numberOfStrokes)
+        {
+            Console.Write("+");
+
+            for (var i = 0; i < numberOfStrokes; i++)
+                Console.Write("-");
+
+            Console.WriteLine("+");
+        }
+    }
+}
