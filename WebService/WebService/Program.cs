d430726584de5b4cf70d540a5de9fe9060487728
Belov-AI <Belov.Alexander@urfu.ru>
2019-12-02 16:22:19 +0500

    Свойсьва. Статические классы

--- WebService/WebService/Program.cs
+++ WebService/WebService/Program.cs
@@ -9,9 +9,41 @@ namespace WebService
         {
             var human = new User();
             human.Login = "Mike";
-            human.Password = "12345";
+            human.Password = "123456";
+            //human.SetPassword("123456");
 
             human.PrintInfo();
+
+            human.Password = "abc";
+            //human.SetPassword("abc");
+
+            human.PrintInfo();
+
+            //Service.Users[0] = human;
+            //Service.NumberOfUsers++;
+
+            Service.AddUser(human);
+
+            //Service.Users[1] = new User() {
+            //    Login = "Kate", Password = "qwerty" };
+            //Service.NumberOfUsers++;
+
+            Service.AddUser(new User()
+            {
+                Login = "Kate",
+                Password = "qwerty"
+            });
+
+            Service.AddUser(new User()
+            {
+                Login = "Nick",
+                Password = "password"
+            });
+
+            Console.WriteLine();
+            for (var i = 0; i < Service.NumberOfUsers; i++)
+                Service.Users[i].PrintInfo();
+
 
             Console.ReadKey();
         }
--- WebService/WebService/User.cs
+++ WebService/WebService/User.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 
-
 namespace WebService
 {
     public class User
     {
         public string Login;
-        public string Password;
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+
+            set
+            {
+                if (value.Length > 5)
+                    password = value;
+                else
+                    Console.WriteLine(
+                        "Длина пароля должа быть не менее 6 знаков");
+            }
+        }
+
         private int userID;
 
 
@@ -15,10 +32,47 @@ namespace WebService
             Console.WriteLine($"Пользователь {Login}. Пароль {Password}");
             //Console.WriteLine("Пользователь {0}. Пароль {1}", Login, Password);
         }
+
+        //public void SetPassword(string newPassword)
+        //{
+        //    if (newPassword.Length > 5)
+        //        Password = newPassword;
+        //    else
+        //        Console.WriteLine(
+        //            "Длина пароля должа быть не менее 6 знаков");
+        //}
     }
 
-    public class Service
+    public static class Service
     {
-        string Protocol;
+        static string Protocol = "TCP/IP";
+
+        static int usercount = 10;
+
+        private static int numerOfUsers = 0;
+
+        public static int NumberOfUsers
+        {
+            get
+            {
+                return numerOfUsers;
+            }
+        }
+
+        private static User[] users = new User[usercount];
+        public static User[] Users
+        {
+            get
+            {
+                return users;
+            }
+        }
+
+        public static void AddUser(User person)
+        {
+            Users[numerOfUsers] = person;
+            numerOfUsers++;
+        }
+
     }
 }
