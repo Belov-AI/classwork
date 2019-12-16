6df32032a4459d0614d63763df3d2fa00ebe8e6d
Belov-AI <Belov.Alexander@urfu.ru>
2019-12-16 16:04:16 +0500

    Добавлен FilesAndDirectorie

---
+++ FilesAndDirectories/FilesAndDirectories/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace FilesAndDirectories
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите имя файла:");
+            var fileName = Console.ReadLine();
+
+            FileStream file;
+
+            if (!File.Exists(fileName))
+                file = File.Create(fileName);
+            else
+                file = new FileStream(fileName, FileMode.Open);
+
+
+            if (File.Exists(file.Name))
+                Console.WriteLine($"Файл {GetShortFileName(file)} существует");
+            else
+                Console.WriteLine($"Файл {GetShortFileName(file)} не существует");
+
+            var anotherFile = new FileInfo("story.txt");
+            Console.WriteLine("Создали объект файла " + anotherFile.Name);
+
+            file.Close();
+            var text = File.ReadAllLines(file.Name);
+            
+            Console.WriteLine();
+
+            foreach (var line in text)
+                Console.WriteLine(line);
+            
+            File.WriteAllText(file.Name,"To be or not to be");
+            var newLines = new[] { "That is the question" };
+            File.AppendAllLines(file.Name, newLines);
+
+
+            Console.ReadKey();
+        }
+
+        static string GetShortFileName(FileStream file)
+        {
+            return file.Name.Substring(file.Name.LastIndexOf('\\')+1);
+        }
+    }
+}
