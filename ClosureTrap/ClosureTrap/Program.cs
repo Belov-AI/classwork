1b5ae664ef594c2623f064eba29eee2a988dd083
Belov-AI <Belov.Alexander@urfu.ru>
2020-05-08 12:37:47 +0500

    Delegates и ClosureTrap

---
+++ ClosureTrap/ClosureTrap/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosureTrap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double scalar = 3;
+            Func<double, double> scale = x => x * scalar;
+
+            Console.WriteLine(scale(5));
+            Console.WriteLine();
+
+            var call = CallCount();
+
+            call();
+            call();
+            call();
+            Console.WriteLine();
+
+            var funcs = new Func<int>[3];
+            //funcs[0] = () => 0;
+            //funcs[1] = () => 1;
+            //funcs[2] = () => 2;
+
+            for (var i = 0; i < funcs.Length; i++)
+                funcs[i] = () => i;
+
+            foreach (var f in funcs)
+                Console.WriteLine(f());
+
+
+            Console.ReadKey();
+        }
+
+        static Action CallCount()
+        {
+            var counter = 0;
+            return () => Console.WriteLine(++counter);
+        }
+    }
+}
---
+++ Delegates/Delegates/Table.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates
+{
+    class Table
+    {
+        public int RowNumber;
+        public double Start;
+        public double Step;
+
+        public void Print(string name, UnaryOperation f)
+        {
+            var horLine = new string('-', 17);
+
+            //шапка таблицы
+            Console.WriteLine(horLine);
+            Console.WriteLine("|   x   | " + name);
+            Console.WriteLine(horLine);
+
+            //печать значений
+            var x = Start;
+            for(var i = 0; i < RowNumber; i++)
+            {
+                Console.WriteLine($"| {x,5:F2} | {f(x):F3}");
+                x += Step;
+            }
+
+            //нижняя линейка и отступ
+            Console.WriteLine(horLine);
+            Console.WriteLine();
+
+        }
+    }
+
+    class Calculator
+    {
+        public double Cube(double x)
+        {
+            return x * x * x;
+        }
+    }
+}
