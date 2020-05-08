752d322014a8751aa6a0dedcc1cc0573ce1d0ae0
Belov-AI <Belov.Alexander@urfu.ru>
2020-05-08 14:08:43 +0500

    Draw - рисование на форме GeometricLibrary - добавление проекта рисования треугольников

---
+++ GeometricLibrary/Drawing/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GeometricLibrary;
+
+namespace Drawing
+{
+    public partial class Form1 : Form
+    {
+        private double ratio = 75;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private System.Drawing.Point ConvertToScreen(GeometricLibrary.Point point)
+        {
+            return new System.Drawing.Point(
+                (int)Math.Round(point.X * ratio),
+                (int)Math.Round(ClientSize.Height - point.Y * ratio));
+        }
+    }
+}
